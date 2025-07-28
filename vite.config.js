38baefebe19e8f499bfdac6b1bbffe0d9a66228c
shitalbhadre <shitalbhadre911@gmail.com>
2025-07-28 12:14:25 +0530

    Fix: added base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/QUIZ_APP/', // 👈 required for GitHub Pages
   plugins: [react()],
 })
